e9a386ce71aafc1679d7e140f3fd87a9c29187aa
Evan Yang <evanyang@pop-os.localdomain>
2020-02-12 13:06:24 -0800

    createHTML function

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 const inquirer = require('inquirer')
+const fs = require('fs')
 
+
+const createHTML = infoObj => {
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+    <meta charset="UTF-8">
+    <title>title</title>
+      </head>
+      <body>
+
+        Name: ${infoObj.name}
+        Location: ${infoObj.location}
+        Bio: ${infoObj.bio}
+        LinkedIn: <a href="${infoObj.linkedIn}">LinkedIn</a>
+        GitHub: <a href="${infoObj.linkedIn}">GitHub</a>
+
+      </body>
+      </html>
+  `
+}
