2e72ccf851a294b92471c6d612323d137ee380b6
Jie Yan <jieyan620@gmail.com>
2020-02-12 12:33:24 -0800

    add prompt

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const inquirer = require('inquirer')
 
+  / inquirer.prompt([
+{
+  type: 'input',
+  name: 'Name',
+  message: 'what is your name?'
+},
+{
+  type: 'input',
+  name: 'Bio',
+  message: 'Please input your bio'
+},
+{
+   type: 'input',
+    name: 'Linkedin',
+    message: 'what is your Linkedin URL?'
+},
+    {
+      type: 'input',
+      name: 'Github',
+      message: 'what is your GitHub URL?'
+    },
+]
+).then(response => {})
